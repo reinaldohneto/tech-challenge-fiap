305180ec2709020550ced3a4b493dfbf6c7c3a51
Henrique Mendes de Brito <hmendesdebrito@outlook.com>
2023-06-01 21:02:54 -0300

    Projeto Web
    Adição do projeto ASP.NET Web na solution

---
+++ Fiap.TechChallenge.WebPage/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Fiap.TechChallenge.WebPage.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
