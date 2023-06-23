0735868074ce060ffa761b4d378e150a0f35b134
Henrique Mendes de Brito <hmendesdebrito@outlook.com>
2023-06-22 23:53:55 -0300

    Adição de barra de pesquisa para memes na tela inicial

--- Fiap.TechChallenge.WebPage/Pages/Index.cshtml.cs
+++ Fiap.TechChallenge.WebPage/Pages/Index.cshtml.cs
@@ -3,6 +3,7 @@ using Fiap.TechChallenge.Api.Application.Services.Memes;
 using Fiap.TechChallenge.WebPage.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json.Linq;
 
 namespace Fiap.TechChallenge.WebPage.Pages
 {
@@ -11,18 +12,56 @@ namespace Fiap.TechChallenge.WebPage.Pages
         private readonly ILogger<IndexModel> _logger;
         private readonly IMemeService _memeFunctionalitiesService;
 
+        private ICollection<MemeDto> _fullMemeList = new List<MemeDto>();
+        [BindProperty]
+        public ICollection<MemeDto> MemeCatalog { get; set; } = new List<MemeDto>();
+
+        private string _filterName = string.Empty;
+        [BindProperty]
+        public string Filter
+        {
+            get => _filterName;
+            set
+            {
+                _filterName = value;
+                RefreshFullMemeList();
+
+                if (!string.IsNullOrEmpty(value))
+                {
+                    var desiredMeme = (from meme in _fullMemeList
+                                       where meme.Name.ToLower().Contains(value.ToLower())
+                                       select meme).ToList();
+
+                    MemeCatalog = desiredMeme;
+                }
+                else
+                    MemeCatalog = _fullMemeList;
+            }
+        }
+
         public IndexModel(ILogger<IndexModel> logger, IMemeService memeFunctionalitiesService)
         {
             _memeFunctionalitiesService = memeFunctionalitiesService;
             _logger = logger;
         }
 
-        public ICollection<MemeDto> MemeCatalog { get; set; } = new List<MemeDto>();
+        private void RefreshFullMemeList()
+        {
+            _fullMemeList = _memeFunctionalitiesService.GetAllMemes().Result;
+        }
 
         public void OnGet()
         {
             MemeCatalog.Clear();
-            MemeCatalog = _memeFunctionalitiesService.GetAllMemes().Result;
+            Filter = string.Empty;
+        }
+
+        public IActionResult OnPostInputValueChanged(string memeName)
+        {
+            Filter = memeName;
+            Console.WriteLine(Filter);
+
+            return Page(); // Optional: Return a specific page or a redirect
         }
     }
 }
